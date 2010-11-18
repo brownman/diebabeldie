bcf4cde56689d6ec4d55ea024e55236414463d6d
Sandro Ribeiro <sandrorib@gmail.com>
2010-11-18 21:29:13 -0200

    first commit \o/

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diebabeldie::Application.config.secret_token = '2357007d5a35a17e01e843346ffa487d439912636f88802935849a24af7521ec4e77fb87850fae772b7eb77989a4ee4e093f291ef6863abd3220834a270d0666'
